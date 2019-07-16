1777f4043245b9a56b3e2e2285d66ecb7a66a56f
Justin Goyena <jgoyen@Justins-MBP.attlocal.net>
2019-07-16 01:13:04 -0700

    added if else statements for user score and random number, fixed score tracker and number resets

---
+++ assets/javascript/game.js
@@ -0,0 +1,71 @@
+var wins = 0;
+var losses = 0;
+
+var min = 19;
+var max = 120;
+var randomNumber = rNum(min,max);
+
+var uScore = 0;
+console.log(uScore);
+
+var bitcoinVal = Math.ceil(Math.random()*12);
+var litecoinlVal = Math.ceil(Math.random()*12);
+var dogecoinVal = Math.ceil(Math.random()*12);
+var rippleVal = Math.ceil(Math.random()*12);
+
+function rNum(min,max){
+return Math.floor(Math.random()*(max-min+1)+min);
+}
+console.log(randomNumber);
+
+$('')
+$('#userScore').text(uScore);
+
+$('#ranNum').text(randomNumber);
+
+$('#wins').text(wins);
+$('#losses').text(losses);
+
+$('img').eq(0).on('click', function(){
+    
+    $('#userScore').text(uScore+=bitcoinVal);
+});
+
+$('img').eq(1).on('click', function(){
+
+    $('#userScore').text(uScore+=litecoinlVal);
+});
+
+$('img').eq(2).on('click', function(){
+
+    $('#userScore').text(uScore+=dogecoinVal);
+});
+
+$('img').eq(3).on('click', function(){
+
+    $('#userScore').text(uScore+=rippleVal);
+});
+
+$(document).on('click', function(){
+    if (uScore == randomNumber){
+
+        $('#wins').text(wins++);
+        gameSetup();
+
+    }else if (uScore > randomNumber){
+        
+        $('#losses').text(losses++);
+        uScore = 0;
+        $('#userScore').text(uScore);
+ 
+    }
+})
+
+function gameSetup(){
+    uScore = 0;
+    $('#userScore').text(uScore);
+
+    randomNumber = rNum(min,max);
+    rNum();
+    $('#ranNum').text(randomNumber);
+}
